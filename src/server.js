867c94784911345ee27528ae039b1c632b413768
jacko2611 <jacksonkent70@gmail.com>
2023-05-31 21:58:42 +1000

    altered server

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
-const dotenv = require ('dotenv');
-
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
+
 dotenv.config();
 
 const app = express();
-
 const apiToken = process.env.API_TOKEN;
 const apiKey = process.env.API_KEY;
 
 app.use(cors());
 
-const baseUrl = "https://ap-southeast-2.api.vaultre.com.au/api"
-
-const initialEndpoint = `${baseUrl}/v1.2/properties`
+const baseUrl = "https://ap-southeast-2.api.vaultre.com.au/api";
+const initialEndpoint = `${baseUrl}/v1.2/properties`;
 
 app.get('/properties', async (req, res) => {
-
   try {
     const response = await fetch(initialEndpoint, {
       headers: {
@@ -25,9 +22,6 @@ app.get('/properties', async (req, res) => {
         "X-Api-Key": apiKey
       }
     });
-    console.log({ apiKey, apiToken });
-    
-
     const data = await response.json();
     res.json(data);
   } catch (error) {
@@ -36,52 +30,26 @@ app.get('/properties', async (req, res) => {
   }
 });
 
-
-app.get('/properties/residential/sale/id', async (req, res) => {
-
+app.get('/properties/residential/sale/:id', async (req, res) => {
   try {
-    const response = await fetch(initialEndpoint, {
+    const propertyId = req.params.id;
+    const propertyEndpoint = `${baseUrl}/v1.2/properties/residential/sale/${propertyId}`;
+    
+    const response = await fetch(propertyEndpoint, {
       headers: {
         "Authorization": `Bearer ${apiToken}`,
         "X-Api-Key": apiKey
       }
-      .then(response => response.json())
-      .then(data => {
-        const propertyIds = data.id;
-
-        const propertyDataPromises = propertyIds.map(propertyId => {
-          const propertyEndpoint = `${baseUrl}/v1.2/properties/residential/sale/${propertyId}`;
-          return fetch(propertyEndpoint)
-            .then(response => response.json())  
-        })
-      })
     });
 
-    Promise
-      .then(propertyData => {
-        propertyData.forEach(propertyInfo => {
-          const propertyId = propertyInfo.id;
-          const propertyAddress = propertyInfo.address;
-          const propertyPrice = propertyInfo.price;
-          const propertyImage = propertyInfo.images[0];
-          const propertyDescription = propertyInfo.description;
-        })
-      })
-      .catch(error => console.log(error));
-        console.error("Error:", error);
-
-
-    console.log({ apiKey, apiToken });
-    
-
-    const data = await response.json();
-    res.json(data);
+    const propertyInfo = await response.json();
+    // Process propertyInfo and send the necessary data in the response
+    res.json(propertyInfo);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "An error occurred while fetching the properties." });
+    res.status(500).json({ message: "An error occurred while fetching the property details." });
   }
 });
-
 
 const port = 3000;
 app.listen(port, () => {
