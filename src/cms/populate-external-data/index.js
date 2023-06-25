dd19f844924efdc6e4b90e992eb917cb68184d34
jacko2611 <jacksonkent70@gmail.com>
2023-06-25 20:35:17 +1000

    commit

--- src/cms/populate-external-data/index.js
+++ src/cms/populate-external-data/index.js
@@ -1,97 +1,207 @@
-const fetchSaleProperties = async () => {
-    try {
-      const response = await fetch("https://realestateserver-fuhd.onrender.com/properties/residential/sale");
-      const data = await response.json();
-      return data.items; 
-    } catch (error) {
-      console.log('Error fetching sale properties');
-      return [];
-    }
-  };
+// const fetchSaleProperties = async () => {
+//     try {
+//       const response = await fetch("https://realestateserver-fuhd.onrender.com/properties/residential/sale");
+//       const data = await response.json();
+//       return data.items; 
+//     } catch (error) {
+//       console.log('Error fetching sale properties');
+//       return [];
+//     }
+//   };
   
-  const fetchSalePropertiesById = async (id) => {
-    try {
-      const response = await fetch(`https://realestateserver-fuhd.onrender.com/properties/residential/sale/${id}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log('Error fetching the sale property');
-      return null;
-    }
-  };
+//   const fetchSalePropertiesById = async (id) => {
+//     try {
+//       const response = await fetch(`https://realestateserver-fuhd.onrender.com/properties/residential/sale/${id}`);
+//       const data = await response.json();
+//       return data;
+//     } catch (error) {
+//       console.log('Error fetching the sale property');
+//       return null;
+//     }
+//   };
   
-  const createPropertyCards = async () => {
-    try {
-      const properties = await fetchSaleProperties();
+//   const createPropertyCards = async () => {
+//     try {
+//       const properties = await fetchSaleProperties();
   
-      if (!Array.isArray(properties)) {
-        console.log('Invalid response format');
-        return;
-      }
+//       if (!Array.isArray(properties)) {
+//         console.log('Invalid response format');
+//         return;
+//       }
   
-      const templateElement = document.getElementById('card-container');
-      const containerElement = templateElement.parentElement;
+//       const templateElement = document.getElementById('card-container');
+//       const containerElement = templateElement.parentElement;
   
-      for (const property of properties) {
-        try {
-          const propertyData = await fetchSalePropertiesById(property.id);
+//       for (const property of properties) {
+//         try {
+//           const propertyData = await fetchSalePropertiesById(property.id);
   
-          if (!propertyData) {
-            console.log(`Property with ID ${property.id} not found`);
-            continue;
-          }
+//           if (!propertyData) {
+//             console.log(`Property with ID ${property.id} not found`);
+//             continue;
+//           }
   
-          const newItem = templateElement.cloneNode(true);
-          newItem.removeAttribute('hidden');
+//           const newItem = templateElement.cloneNode(true);
+//           newItem.removeAttribute('hidden');
   
         
-          const titleElement = newItem.querySelector('#address');
-          if (titleElement) {
-            titleElement.textContent = propertyData.displayAddress;
-          }
+//           const titleElement = newItem.querySelector('#address');
+//           if (titleElement) {
+//             titleElement.textContent = propertyData.displayAddress;
+//           }
   
-          const priceElement = newItem.querySelector('#price');
-          if (priceElement) {
-            priceElement.textContent = propertyData.salePrice;
-          }
+//           const priceElement = newItem.querySelector('#price');
+//           if (priceElement) {
+//             priceElement.textContent = propertyData.salePrice;
+//           }
   
-          const bedElement = newItem.querySelector('#bedroom');
-          if (bedElement) {
-            bedElement.textContent = propertyData.bed;
-          }
+//           const bedElement = newItem.querySelector('#bedroom');
+//           if (bedElement) {
+//             bedElement.textContent = propertyData.bed;
+//           }
   
-          const bathElement = newItem.querySelector('#bathroom');
-          if (bathElement) {
-            bathElement.textContent = propertyData.bath;
-          }
+//           const bathElement = newItem.querySelector('#bathroom');
+//           if (bathElement) {
+//             bathElement.textContent = propertyData.bath;
+//           }
   
-          const imageElement = newItem.querySelector('#image');
-          if (imageElement) {
-            const primaryPhoto = propertyData.photos.find((photo) => photo.isPrimary);
-            if (primaryPhoto) {
-              imageElement.src = primaryPhoto.url;
-              imageElement.alt = primaryPhoto.description;
-            }
-          }
+//           const imageElement = newItem.querySelector('#image');
+//           if (imageElement) {
+//             const primaryPhoto = propertyData.photos.find((photo) => photo.isPrimary);
+//             if (primaryPhoto) {
+//               imageElement.src = primaryPhoto.url;
+//               imageElement.alt = primaryPhoto.description;
+//             }
+//           }
   
           
-          newItem.addEventListener('click', () => {
-            const descriptionElement = newItem.querySelector('#description');
-            if (descriptionElement) {
-              descriptionElement.textContent = propertyData.description;
-            }
-          });
+//           newItem.addEventListener('click', () => {
+//             const descriptionElement = newItem.querySelector('#description');
+//             if (descriptionElement) {
+//               descriptionElement.textContent = propertyData.description;
+//             }
+//           });
   
          
-          containerElement.appendChild(newItem);
-        } catch (error) {
-          console.log(`Error creating property card for ID ${property.id}:`, error);
-        }
-      }
-    } catch (error) {
-      console.log('Error creating property cards:', error);
+//           containerElement.appendChild(newItem);
+//         } catch (error) {
+//           console.log(`Error creating property card for ID ${property.id}:`, error);
+//         }
+//       }
+//     } catch (error) {
+//       console.log('Error creating property cards:', error);
+//     }
+//   };
+  
+//   createPropertyCards();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const fetchSaleProperties = async () => {
+  try {
+    const response = await fetch("https://realestateserver-fuhd.onrender.com/properties/residential/sale");
+    const data = await response.json();
+    return data.items; 
+  } catch (error) {
+    console.log('Error fetching sale properties');
+    return [];
+  }
+};
+
+const fetchSalePropertiesById = async (id) => {
+  try {
+    const response = await fetch(`https://realestateserver-fuhd.onrender.com/properties/residential/sale/available`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error fetching the sale property');
+    return null;
+  }
+};
+
+const createPropertyCards = async () => {
+  try {
+    const properties = await fetchSaleProperties();
+
+    if (!Array.isArray(properties)) {
+      console.log('Invalid response format');
+      return;
     }
-  };
-  
-  createPropertyCards();
-  
+
+    const templateElement = document.getElementById('card-container');
+    const containerElement = templateElement.parentElement;
+
+    for (const property of properties) {
+      try {
+        const propertyData = await fetchSalePropertiesById(property.id);
+
+        if (!propertyData) {
+          console.log(`Property with ID ${property.id} not found`);
+          continue;
+        }
+
+        const newItem = templateElement.cloneNode(true);
+        newItem.removeAttribute('hidden');
+
+      
+        const titleElement = newItem.querySelector('#address');
+        if (titleElement) {
+          titleElement.textContent = propertyData.displayAddress;
+        }
+
+        const priceElement = newItem.querySelector('#price');
+        if (priceElement) {
+          priceElement.textContent = propertyData.salePrice;
+        }
+
+        const bedElement = newItem.querySelector('#bedroom');
+        if (bedElement) {
+          bedElement.textContent = propertyData.bed;
+        }
+
+        const bathElement = newItem.querySelector('#bathroom');
+        if (bathElement) {
+          bathElement.textContent = propertyData.bath;
+        }
+
+        const imageElement = newItem.querySelector('#image');
+        if (imageElement) {
+          const primaryPhoto = propertyData.photos.find((photo) => photo.isPrimary);
+          if (primaryPhoto) {
+            imageElement.src = primaryPhoto.url;
+            imageElement.alt = primaryPhoto.description;
+          }
+        }
+
+        
+        newItem.addEventListener('click', () => {
+          const descriptionElement = newItem.querySelector('#description');
+          if (descriptionElement) {
+            descriptionElement.textContent = propertyData.description;
+          }
+        });
+
+       
+        containerElement.appendChild(newItem);
+      } catch (error) {
+        console.log(`Error creating property card for ID ${property.id}:`, error);
+      }
+    }
+  } catch (error) {
+    console.log('Error creating property cards:', error);
+  }
+};
+
+createPropertyCards();
