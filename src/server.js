d42e044ecfeeffa0addb2e7588b0623d64134597
jacko2611 <jacksonkent70@gmail.com>
2023-09-17 22:17:09 +1000

    y

--- src/server.js
+++ src/server.js
@@ -109,23 +109,25 @@ app.get('/properties/residential/sale/available', async (req, res) => {
         "X-Api-Key": apiKey
       }
     });
-    
+
     if (!response.ok) {
       throw new Error('Failed to fetch data from the external API');
     }
 
-    const data = await response.json();
+    const responseData = await response.json(); // Assuming 'photos' is the top-level field
 
-    // Extract photo URLs and format them as a comma-separated string
-    const photoUrls = data.photos.map(photo => photo.url).join(',');
+    // Check if 'photos' is an array before proceeding
+    if (Array.isArray(responseData.photos)) {
+      // Extract photo URLs and format them as a comma-separated string
+      const photoUrls = responseData.photos.map(photo => photo.url).join(',');
 
-    // Create a new object with the formatted photo URLs
-    const responseData = {
-      ...data, // Include the original data
-      photos: photoUrls // Replace the 'photos' property with the formatted string
-    };
+      // Update the 'photos' property in responseData with the formatted string
+      responseData.photos = photoUrls;
 
-    res.json(responseData);
+      res.json(responseData);
+    } else {
+      throw new Error('Invalid data format from the external API');
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "An error occurred while fetching the properties." });
